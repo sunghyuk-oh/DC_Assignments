3867680d4e2fb975357aa6bdc85cc4af7111f113
Tim Oh <timoh@Tims-MacBook-Pro.local>
2021-07-27 17:00:17 -0400

    javascript assignment added

---
+++ JavaScript/Day_2/app.js
@@ -0,0 +1,53 @@
+const taskTitle = document.getElementById("taskTitle")
+const addButton = document.getElementById("addButton")
+const boxContainer = document.getElementById("box-container")
+const boxContainer2 = document.getElementById("box-container2")
+
+// Trigger the Add button
+addButton.addEventListener("click", function() {
+    task = taskTitle.value
+
+    // create child div
+    const taskDiv = document.createElement("div")
+    taskDiv.setAttribute("class", "box")
+
+    // create "input" and assign attribute (type / "checkbox")
+    const taskInput = document.createElement("input")
+    taskInput.setAttribute("type", "checkbox")
+
+    // create "label" and assign attribute (class) & and add task title into the label
+    const taskLabel = document.createElement("label")
+    taskLabel.setAttribute("class", "taskTitle")
+    taskLabel.innerHTML = task
+
+    // create "button" and add text inside the button tag
+    const taskRemoveButton = document.createElement("button")
+    taskRemoveButton.innerHTML = "Remove"
+
+
+    // Add input,label,button into child div
+    taskDiv.appendChild(taskInput)
+    taskDiv.appendChild(taskLabel)
+    taskDiv.appendChild(taskRemoveButton)
+
+    // add child div into parent div
+    boxContainer.appendChild(taskDiv)
+
+
+    // triggering the remove button
+    taskRemoveButton.addEventListener("click", function() {
+        this.parentElement.parentElement.removeChild(this.parentElement)
+    })
+
+
+    // triggering the checkbox
+    taskInput.addEventListener("change", function() {
+        if (taskInput.checked) {
+            // boxContainer.removeChild(taskDiv)
+            boxContainer2.appendChild(taskDiv)
+        } else {
+            // boxContainer2.removeChild(taskDiv)
+            boxContainer.appendChild(taskDiv)
+        }
+    })
+})
