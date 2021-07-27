2a100ad0c1f67580e28848cbed868f4a16a72c88
Tim Oh <timoh@Tims-MacBook-Pro.local>
2021-07-27 17:19:18 -0400

    JavaScript assignment added

--- JavaScript/Day_2/app.js
+++ JavaScript/Day_2/app.js
@@ -6,48 +6,51 @@ const boxContainer2 = document.getElementById("box-container2")
 // Trigger the Add button
 addButton.addEventListener("click", function() {
     task = taskTitle.value
+    if (task.length == 0) {
+        alert("The text box is empty. Please enter the task title.")
+    } else {
+        // create child div
+        const taskDiv = document.createElement("div")
+        taskDiv.setAttribute("class", "box")
 
-    // create child div
-    const taskDiv = document.createElement("div")
-    taskDiv.setAttribute("class", "box")
+        // create "input" and assign attribute (type / "checkbox")
+        const taskInput = document.createElement("input")
+        taskInput.setAttribute("type", "checkbox")
 
-    // create "input" and assign attribute (type / "checkbox")
-    const taskInput = document.createElement("input")
-    taskInput.setAttribute("type", "checkbox")
+        // create "label" and assign attribute (class) & and add task title into the label
+        const taskLabel = document.createElement("label")
+        taskLabel.setAttribute("class", "taskTitle")
+        taskLabel.innerHTML = task
 
-    // create "label" and assign attribute (class) & and add task title into the label
-    const taskLabel = document.createElement("label")
-    taskLabel.setAttribute("class", "taskTitle")
-    taskLabel.innerHTML = task
-
-    // create "button" and add text inside the button tag
-    const taskRemoveButton = document.createElement("button")
-    taskRemoveButton.innerHTML = "Remove"
-
-
-    // Add input,label,button into child div
-    taskDiv.appendChild(taskInput)
-    taskDiv.appendChild(taskLabel)
-    taskDiv.appendChild(taskRemoveButton)
-
-    // add child div into parent div
-    boxContainer.appendChild(taskDiv)
+        // create "button" and add text inside the button tag
+        const taskRemoveButton = document.createElement("button")
+        taskRemoveButton.innerHTML = "Remove"
 
 
-    // triggering the remove button
-    taskRemoveButton.addEventListener("click", function() {
-        this.parentElement.parentElement.removeChild(this.parentElement)
-    })
+        // Add input,label,button into child div
+        taskDiv.appendChild(taskInput)
+        taskDiv.appendChild(taskLabel)
+        taskDiv.appendChild(taskRemoveButton)
+
+        // add child div into parent div
+        boxContainer.appendChild(taskDiv)
 
 
-    // triggering the checkbox
-    taskInput.addEventListener("change", function() {
-        if (taskInput.checked) {
-            // boxContainer.removeChild(taskDiv)
-            boxContainer2.appendChild(taskDiv)
-        } else {
-            // boxContainer2.removeChild(taskDiv)
-            boxContainer.appendChild(taskDiv)
-        }
-    })
+        // triggering the remove button
+        taskRemoveButton.addEventListener("click", function() {
+            this.parentElement.parentElement.removeChild(this.parentElement)
+        })
+
+
+        // triggering the checkbox
+        taskInput.addEventListener("change", function() {
+            if (taskInput.checked) {
+                // boxContainer.removeChild(taskDiv)
+                boxContainer2.appendChild(taskDiv)
+            } else {
+                // boxContainer2.removeChild(taskDiv)
+                boxContainer.appendChild(taskDiv)
+            }
+        })
+    }
 })
