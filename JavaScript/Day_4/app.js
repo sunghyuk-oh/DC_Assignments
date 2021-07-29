de90c2e6d7116d607904e1b77d3887f3d2097720
Tim Oh <timoh@Tims-MacBook-Pro.local>
2021-07-29 18:45:19 -0400

    assignment added

--- JavaScript/Day_4/app.js
+++ JavaScript/Day_4/app.js
@@ -3,46 +3,48 @@ const dropdownMenu = document.getElementById("dropdownMenu")
 const newsInfo = news.articles
 const sourcesInfo = sources.sources
 
-console.log(sourcesInfo[0].name)
-console.log(newsInfo[0].source.id)
-
 
 // Adding the articles into the main content div
-const articleContents = newsInfo.map((article) => {
-    // if (Object.values(article) == null) {
-    //     Object.values(article) = "None"
-    // }
-    return `
-        <div id="article"> 
-            <h2>${article.title}</h2>
-            <p>${article.description}</p>
-            <p>Published At ${article.publishedAt}</p>
-            <p>Author: ${article.author}</p>
-            <img src="${article.urlToImage}">
-            <p><a href="${article.url}">Click here to read the article</a> || <a href="${article.urlToImage}">Click here for the image</a></p>
-        </div>    
-    `
-})
-mainContent.innerHTML = articleContents.join("")
+function display_articles(articles) {
+    const articleContents = articles.map((article) => {
+        // if (Object.values(article) == null) {
+        //     Object.values(article) = "None"
+        // }
+        return `
+            <div id="article"> 
+                <h2>${article.title}</h2>
+                <p>${article.description}</p>
+                <p>Published At ${article.publishedAt}</p>
+                <p>Author: ${article.author}</p>
+                <img src="${article.urlToImage}">
+                <p><a href="${article.url}">Click here to read the article</a> || <a href="${article.urlToImage}">Click here for the image</a></p>
+            </div>    
+        `
+    })
+    mainContent.innerHTML = articleContents.join("")
+}
+
+display_articles(newsInfo)
 
 
-// Creating the source dropdown menu options
+// HARDER MODE
 const dropdownOptions = sourcesInfo.map(function(source) {
     return `<option value="${source.name}">${source.name}</option>`
 })
-dropdownMenu.innerHTML = dropdownOptions.join("")
+dropdownMenu.insertAdjacentHTML('beforeend', dropdownOptions)
 
-
-
-
-const newsSources = newsInfo.map((article) => {
-    return article.source
-})
-const sourcesNames = newsSources.map((name) => {
-    return name.name
-})
-console.log(sourcesNames)
 
 dropdownMenu.addEventListener("change", function(e) {
-    console.log(e.target.value)
+    if (e.target.menu == "All articles") {
+        display_articles(newsInfo)
+    } else {
+        const filterSourcesNames = newsInfo.filter((article) => {
+            return article.source.name === e.target.value
+        })
+        if (filterSourcesNames.length == 0) {
+            alert("The article not found!")
+        } else {
+            display_articles(filterSourcesNames)
+        }
+    }
 })
