e823aa98c7be72326b8e0ea127abc3d6cf451a82
Tim Oh <timoh@Tims-MacBook-Pro.local>
2021-08-05 16:31:19 -0400

    assignment added

--- Asynchronous/Day_4/app.js
+++ Asynchronous/Day_4/app.js
@@ -1,3 +1,12 @@
+// DESCRIPTION 
+// You will use the following service to get all the story IDs.
+// https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty
+// After getting all the story IDs you will use each story ID to fetch the story from using the following end point.
+// https://hacker-news.firebaseio.com/v0/item/8863.json?print=pretty
+// (Plug in the story ID in the place of 8863 in the above example)
+// Display the following information about each story: title / url / by / time
+
+
 const displayNewsUL = document.getElementById("displayNewsUL")
 const ID_URL = 'https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty'
 
